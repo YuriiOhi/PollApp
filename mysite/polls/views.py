3d5d960614cbd83f5b36b383a93564f33c92216e
Yurii Ohiria <ohiriazou@gmail.com>
2024-10-27 16:16:27 -0400

    POL- 0001 - Polls - Add unit tests + add view classes + configure urls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,42 +1,43 @@
 # Importing necessary modules and functions
 from django.db.models import F
-from django.http import Http404
-from .models import Choice, Question
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader   # For loading templates (not used in this code)
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Choice, Question
+ 
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+        :5
+    ]
 
 
-# Importing the Question model from the current app's models.py
-from .models import Question
-
-# The index view: Displays the latest 5 questions
-def index(request):
-    # Query the database to get the latest 5 questions ordered by publication date
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # Create a context dictionary to pass to the template
-    context = {"latest_question_list": latest_question_list}
-    # Render the "polls/index.html" template with the context data
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-
-# The detail view: Displays a specific question based on its ID
-def detail(request, question_id):
-    # Returns a simple text response with the question ID
-    return HttpResponse("You're looking at question %s." % question_id)
-
-# The results view: Displays the results of a specific question
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
     
-    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
 # The vote view: Handles voting on a specific question
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -61,3 +62,8 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 
+# The results view: Displays the results of a specific question
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+    
